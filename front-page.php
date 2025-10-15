ee5bce8f533d427d6abff5daa247b2c5d91ab6e0
Ahmadh24 <ahmadhamoudeh1999@gmail.com>
2025-10-15 10:03:16 -0400

    Increase background height to 550vh for full vertical coverage

--- front-page.php
+++ front-page.php
@@ -12,7 +12,7 @@ get_header(); ?>
 @media screen and (max-width: 768px) {
     html, body {
         background: url('https://youknowitcards.net/wp-content/uploads/2025/09/brickwall.png') center center fixed no-repeat !important;
-        background-size: 100% 150vh !important;
+        background-size: 100% 550vh !important;
         /* min-height: 100vh !important; */
     }
 }
