1b24e33c94a7036daca11bc6742f539e25046acd
Ahmadh24 <ahmadhamoudeh1999@gmail.com>
2025-10-15 09:41:05 -0400

    Update brick background sizing

--- front-page.php
+++ front-page.php
@@ -12,7 +12,7 @@ get_header(); ?>
 @media screen and (max-width: 768px) {
     html, body {
         background: url('https://youknowitcards.net/wp-content/uploads/2025/09/brickwall.png') center center fixed no-repeat !important;
-        background-size: 80% auto !important;
+        background-size: 110% auto !important;
         min-height: 100vh !important;
     }
 }
