b707ec9e60a8cad340abbf0011ade5d30810b8d5
Ahmadh24 <ahmadhamoudeh1999@gmail.com>
2025-10-17 22:12:21 -0400

    Adjust button positioning to translateY(150px)

--- front-page.php
+++ front-page.php
@@ -39,7 +39,7 @@ get_header(); ?>
     .hero-buttons {
         margin-top: -400px !important;
         margin-bottom: 30px !important;
-        transform: translateY(250px) !important;
+        transform: translateY(150px) !important;
         position: relative !important;
         z-index: 999 !important;
     }
